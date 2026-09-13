3fb2f7590cc3e51d313f899afc0caef8ab520111
sandro Ribeiro <sandromarcioribeiro@gmail.com>
2026-09-13 19:49:31 -0300

    Simplify workbench startup registration

--- InitGui.py
+++ InitGui.py
@@ -1,22 +1,11 @@
 """GUI registration for the Plasma CNC workbench."""
 
-import os
-
-import FreeCAD as App
 import FreeCADGui as Gui
-
-
-def _workbench_dir():
-    loaded_file = globals().get("__file__")
-    if loaded_file:
-        return os.path.dirname(os.path.abspath(loaded_file))
-    return os.path.join(App.getUserAppDataDir(), "Mod", "PlasmaCNCWorkbench")
 
 
 class PlasmaCNCWorkbench(Gui.Workbench):
     MenuText = "Plasma CNC"
     ToolTip = "Preparar cortes de plasma CNC e gerar G-code Mach3"
-    Icon = os.path.join(_workbench_dir(), "resources", "plasma_cnc.svg")
 
     def Initialize(self):
         from plasma_cnc.commands import PlasmaCNCGenerateSampleCommand, PlasmaCNCShowGuideCommand
